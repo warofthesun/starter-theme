36191372a843405c15cda0b3aa56fa2aa4a77279
Dan Brown <fireflip@gmail.com>
2016-12-11 13:55:48 -0600

    Remove Lato from functions.php and styles

--- functions.php
+++ functions.php
@@ -237,11 +237,11 @@ twentythirteen theme where we can declare some
 external fonts. If you're using Google Fonts, you
 can replace these fonts, change it in your scss files
 and be up and running in seconds.
-*/
+
 function bones_fonts() {
   wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
 }
 
 add_action('wp_enqueue_scripts', 'bones_fonts');
-
+*/
 /* DON'T DELETE THIS CLOSING TAG */ ?>
