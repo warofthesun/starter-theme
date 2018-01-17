81451447dc51657f280ec7617e6394c1c02475a8
Daniel Brown <fireflip@gmail.com>
2018-01-16 22:28:48 -0600

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('watch', function(){
+  gulp.watch('library/scss/**/*.scss', ['sass']);
+  // Other watchers
+})
+
+gulp.task('sass', function(){
+  return gulp.src('library/scss/**/*.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(sass().on('error', sass.logError))
+    //.pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+    .pipe(gulp.dest('./'))
+});
